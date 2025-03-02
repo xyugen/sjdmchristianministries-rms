50f01245f9e170a89ce123777ac773dda49220e4
xyugen <alex.arias.jorym@gmail.com>
2025-03-02 20:32:02 +0800

    feat: improve login page and add meta titles

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -10,18 +10,18 @@ export const metadata: Metadata = {
 
 const Page = () => {
   return (
-    <div className="mx-auto w-full max-w-md">
-      <div className="my-2 flex flex-col gap-2 rounded bg-blue-gem-900 p-2">
+    <div className="bg-white p-4 rounded mx-auto w-full max-w-md">
+      <div className="my-2 flex flex-col gap-2 rounded-sm bg-blue-gem-900 p-2">
         <Image
           src={Logo}
           alt="Logo"
           width={200}
           height={200}
-          className="mx-auto size-20 rounded-sm"
+          className="mx-auto size-20"
         />
 
         <h2 className="text-center text-xl font-bold leading-tight text-foreground !text-blue-gem-100 sm:text-2xl">
-          SJDM Christian Ministries
+          SJDM Christian Ministry
         </h2>
       </div>
 
