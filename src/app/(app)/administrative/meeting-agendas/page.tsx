09b8db418402c53254d67df51afca9e038e3af8d
Dochiibells <villarcarlogian14@gmail.com>
2025-03-03 22:54:56 +0800

    feat(policies & agendas): add page

---
+++ src/app/(app)/administrative/meeting-agendas/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const metadata = {
+  title: "Meeting Agendas",
+};
+
+const Page = () => {
+  return (
+    <div className="flex flex-col">
+      <h1 className="text-2xl font-bold">Meeting Agendas</h1>
+    </div>
+  );
+};
+
+export default Page;
