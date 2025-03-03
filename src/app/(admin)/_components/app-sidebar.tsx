2fcacc28b7395d7fd716c90d076653efba72091f
Kaizouku14 <mandaalv72@gmail.com>
2025-03-03 15:24:42 +0800

    feat(admin): add menus in sidebar

--- src/app/(admin)/_components/app-sidebar.tsx
+++ src/app/(admin)/_components/app-sidebar.tsx
@@ -1,3 +1,10 @@
+import { Home, ChevronRight, FolderDown } from "lucide-react";
+
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
 
 import {
   Sidebar,
@@ -5,34 +12,89 @@ import {
   SidebarGroup,
   SidebarGroupContent,
   SidebarGroupLabel,
+  SidebarHeader,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
-} from "@/components/ui/sidebar"
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
+} from "@/components/ui/sidebar";
 
-// Menu items.
-const items = [
+import { PageRoutes } from "@/constants/page-routes";
+
+const RecordMenuItems = [
   {
-    title: "Home",
-    url: "#",
-    icon: "",
+    title: "Records",
+    icon: FolderDown,
+    isActive: false,
+    items: [
+      {
+        title: "Administrative",
+        url: "#",
+      },
+      {
+        title: "Human Resources",
+        url: "#",
+      },
+      {
+        title: "Financial",
+        url: "#",
+      },
+    ],
   },
- 
-]
+];
 
 export function AppSidebar() {
   return (
     <Sidebar>
-      <SidebarContent>
-        <SidebarGroup>
-          <SidebarGroupLabel>Application</SidebarGroupLabel>
-          <SidebarGroupContent>
-            <SidebarMenu>
-             
-            </SidebarMenu>
-          </SidebarGroupContent>
+      <SidebarHeader className="text-lg text-center font-medium p-1.5">SJDM Christian Ministry</SidebarHeader>
+      <SidebarContent className="mt-4">
+        <SidebarGroup className="text-base pb-0">
+          <SidebarMenuButton asChild>
+            <a href={PageRoutes.DASHBOARD}>
+              <Home />
+              <span>Dashboard</span>
+            </a>
+          </SidebarMenuButton>
+        </SidebarGroup>
+
+        <SidebarGroup className="text-base pt-0">
+          <SidebarMenu>
+            {RecordMenuItems.map((item) => (
+              <Collapsible
+                key={item.title}
+                asChild
+                defaultOpen={item.isActive}
+                className="group/collapsible"
+              >
+                <SidebarMenuItem>
+                  <CollapsibleTrigger asChild>
+                    <SidebarMenuButton tooltip={item.title}>
+                      {item.icon && <item.icon />}
+                      <span>{item.title}</span>
+                      <ChevronRight className="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
+                    </SidebarMenuButton>
+                  </CollapsibleTrigger>
+                  <CollapsibleContent>
+                    <SidebarMenuSub>
+                      {item.items?.map((subItem) => (
+                        <SidebarMenuSubItem key={subItem.title}>
+                          <SidebarMenuSubButton asChild>
+                            <a href={subItem.url}>
+                              <span>{subItem.title}</span>
+                            </a>
+                          </SidebarMenuSubButton>
+                        </SidebarMenuSubItem>
+                      ))}
+                    </SidebarMenuSub>
+                  </CollapsibleContent>
+                </SidebarMenuItem>
+              </Collapsible>
+            ))}
+          </SidebarMenu>
         </SidebarGroup>
       </SidebarContent>
     </Sidebar>
-  )
+  );
 }
--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,5 +1,10 @@
+import { type Metadata } from 'next';
 import React from 'react'
 
+export const metadata: Metadata = {
+    title: "Dashboard",
+  };
+  
 const Page = () => {
   return (
     <div>Page</div>
