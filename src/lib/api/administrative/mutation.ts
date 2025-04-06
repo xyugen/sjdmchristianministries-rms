03871be40ecc0a64f551a250a4cc2c44bcad640a
Darvey Trinidad <trinidaddarvey10@gmail.com>
2025-04-06 23:16:34 +0800

    Refactor: Add returning clause to organizational policy mutations

--- src/lib/api/administrative/mutation.ts
+++ src/lib/api/administrative/mutation.ts
@@ -4,28 +4,37 @@ import { eq } from "@/server/db";
 import { generateUUID } from "@/lib/utils";
 
 
-export const createOrganizationalPolicy = async ( organizationalPolicy ) => {
+export const createOrganizationalPolicy = async (organizationalPolicy) => {
   try {
-    await db.insert(organizationalPolicies).values({
+    return await db.insert(organizationalPolicies).values({
       id: generateUUID(),
       ...organizationalPolicy
-    }).returning().run();
+    })
+      .returning()
+      .run();
   } catch (error) {
     console.log(error);
   }
 }
 
-export const editOrganizationalPolicy = async (id : string, organizationalPolicy) => {
+export const editOrganizationalPolicy = async (id: string, organizationalPolicy: { title?: string; description?: string }) => {
   try {
-    await db.update(organizationalPolicies).set(organizationalPolicy).where(eq(organizationalPolicies.id, id)).run();
+    return await db.update(organizationalPolicies)
+      .set(organizationalPolicy)
+      .where(eq(organizationalPolicies.id, id))
+      .returning()
+      .run();
   } catch (error) {
     console.log(error);
   }
 }
 
-export const deleteOrganizationalPolicy = async (id : string) => {
+export const deleteOrganizationalPolicy = async (id: string) => {
   try {
-    await db.delete(organizationalPolicies).where(eq(organizationalPolicies.id, id)).run();
+    return await db.delete(organizationalPolicies)
+      .where(eq(organizationalPolicies.id, id))
+      .returning()
+      .run();
   } catch (error) {
     console.log(error);
   }
