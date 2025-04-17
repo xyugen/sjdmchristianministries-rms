a836a1cdb16722419652ffd3054be203da5a00e2
Dochiibells <villarcarlogian14@gmail.com>
2025-04-18 00:35:38 +0800

    add stat table in dashboard

--- src/app/(app)/financial/page.tsx
+++ src/app/(app)/financial/page.tsx
@@ -1,10 +1,13 @@
 
-import PageBreadCrumb from "@/components/breadcrumb/page-breadcrumb";
+import BreadcrumbLayout from "@/components/breadcrumb/page-breadcrumb";
 import React from "react";
 
 const Page = () => {
   return (
-    <div>
+    <div className="flex flex-col">
+      <BreadcrumbLayout
+        currentPage="Financial Records"
+      />
       
     </div>
   )
