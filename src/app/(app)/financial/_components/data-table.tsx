a247542c5b09df724f6bad55ad0eba1406c6b9df
Dochiibells <villarcarlogian14@gmail.com>
2025-04-21 22:52:08 +0800

    fix financial table

--- src/app/(app)/financial/_components/data-table.tsx
+++ src/app/(app)/financial/_components/data-table.tsx
@@ -1,15 +1,19 @@
+"use client";
 import React from "react";
 import { columns } from "./columns";
 import { DataTable } from "@/components/table/data-table";
 import { DataTableSkeleton } from "@/components/table/data-table-skeletion";
-import { TransactionOption, type Transaction } from "@/constants/transaction";
+import { TransactionOption } from "@/constants/transaction";
 import { api } from "@/trpc/react";
 
 
 const TransactionsTable = () => {
 
-  const { data, isLoading } = 
+  const { data, isLoading, error } = 
   api.finance.getAllFinancialTransactions.useQuery();
+
+  console.log(data);
+  console.log(error);
 
   return (
     <div className="mt-6">
