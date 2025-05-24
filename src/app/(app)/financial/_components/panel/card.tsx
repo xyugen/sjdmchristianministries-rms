e34b5dd4061986d1c2bd242f960d427b959dc884
Dochiibells <villarcarlogian14@gmail.com>
2025-05-24 21:28:56 +0800

    add(policies-procedures):edit and delete

---
+++ src/app/(app)/financial/_components/panel/card.tsx
@@ -0,0 +1,28 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { api } from "@/trpc/react";
+import { HandCoins } from "lucide-react";
+export const BalanceCard = () => {
+  // Temporary data lang to alv, ayusin mo to
+  const data = 12345.67;
+
+  return (
+    <Card className="my-3">
+      <CardHeader>
+        <CardTitle className="flex items-center gap-2">
+          <HandCoins className="size-6" />
+          Current Balance
+        </CardTitle>
+      </CardHeader>
+      <CardContent>
+        <div className="flex flex-row items-center">
+          <div className="text-3xl font-semibold">
+            {data.toLocaleString("en-PH", {
+              style: "currency",
+              currency: "PHP",
+            })}
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
